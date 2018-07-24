4c9357d619159e9c7e334ac511247415799bdcf5
chandnipatel881 <chandni.patel881@gmail.com>
2018-07-24 14:17:01 -0700

    Adding Tableau Class

---
+++ Proj3/src/test/java/TableauTest.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TableauTest {
+
+    public Tableau tableau = new Tableau(5,5);
+
+    @Test
+    public void testTableauConstructor(){
+        System.out.println(tableau);
+    }
+
+    @Test
+    public void testConsolidate() {
+        System.out.println(tableau);
+        tableau.tableau[4] = null;
+        tableau.tableau[5] = null;
+        tableau.tableau[11] = null;
+        tableau.tableau[13] = null;
+        tableau.consolidate();
+        System.out.println(tableau);
+    }
+}
